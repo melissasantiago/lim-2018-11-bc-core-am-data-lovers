491d23c4031d3786269a0168bac09b765d447393
melissasantiago <meli235ssa@gmail.com>
2018-12-14 10:05:13 -0500

    array indicators and html

---
+++ src/main.js
@@ -0,0 +1,25 @@
+//console.table(WORLDBANK.per);
+
+//paa convertir a ARRAY en la consola
+const newWorld = (Object.keys(WORLDBANK.PER.indicators[0]));
+
+//const newWorld2 = newWorld.
+  //for(let i = 0; i< newWorld.length; i++){
+//const filPer = newWorld.filter(newWorld => newWorld.length >0)
+  console.log(newWorld);// aparece en consola
+//}
+//para hacer una copia
+
+/* const copyObj = (X) => {
+  let newObj =[];
+
+  for(let i = 0; i < X.length; i++){
+    console.log(x[i])
+
+    newObj.push(Object.assign({},X[i]))
+
+  }return newObj
+
+}
+
+copyObj(newWorld);*/
