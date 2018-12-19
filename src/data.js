1e9872a93d14d53532737906d8a26d46774b3885
melissa <meli235ssa@gmail.com>
2018-12-19 13:03:36 -0500

    array indicadores peru consola

--- src/data.js
+++ src/data.js
@@ -1,43 +1,50 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-//Convertir a Array
-//console.log(Object.entries(WORLDBANK))
+// Convertir a Array
+// console.log(Object.entries(WORLDBANK))
 const allTheData = Object.entries(WORLDBANK);
-console.log(allTheData)
-/*const data = WORLDBANK.value;
+console.log(allTheData);
+/* const data = WORLDBANK.value;
 const newDataBank = []*/
 
-//Clona toda la data para solo usar dataClone
+// Clona toda la data para solo usar dataClone
 const dataClone = (dataBank) => {
   const newDataBank = [];
   for (let i = 0; i < dataBank.length; i++){
-    //newDataBank.push(dataBank[i])
-    console.log(dataBank[i])
-    newDataBank.push(Object.assign({},dataBank[i]))
-  }
+// newDataBank.push (dataBank[i])
+  newDataBank.push ( Object.assign({},dataBank [i]))
+}
   return newDataBank;
 }
-//Esto funciona solo con la funcion de arriba
-//dataClone(allTheData);
 
-//const example = () => {
-  //return 'example';
-//};
+const data = dataClone(allTheData) // data es una array con 4 objetos (cada objeto es un pais)
 
-function filterData(PER) {
-  return PER === [5];
+// const peruInd = (dataPeru) => {
+//   const newInd = [];
+//   for (let i = 0; i <dataPeru.length; i++){
+// console.log(dataPeru[i])
+//   newInd(Object.values(WORLDBANK.peru))
+// }
+// return newInd;
+// }
+
+
+const getDataPeru = (arrayDePaises) => {
+  const dataDePeru = arrayDePaises[0]; // un objeto con 2 propiedades que tiene la data de peru
+  return dataDePeru['1'].indicators;
 }
- const filterList = (PER, indicatorName ) => {
- const indicatorsList = PER.indicatorName.filter(filterData);
- }
 
- console.log(indicatorsList);
+const arrayDeIndicadoresDePeru = getDataPeru(data);
 
+console.log(arrayDeIndicadoresDePeru)
+// Esto funciona solo con la funcion de arriba
+// dataClone(allTheData);
 
-//sortData(data, sortBy, sortOrder)
-//computeStats(data)
+// const example = () => {
+// return 'example';
+// };
+
 window.data = {
-  filterData:filterData,
-  //sortData:sortData
+
 };
