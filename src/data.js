b1a21a12bf72dff31a2745240cef5d3656f4fb46
melissa <meli235ssa@gmail.com>
2018-12-20 00:35:32 -0500

    indicadores peru en html

--- src/data.js
+++ src/data.js
@@ -20,16 +20,6 @@ const dataClone = (dataBank) => {
 
 const data = dataClone(allTheData) // data es una array con 4 objetos (cada objeto es un pais)
 
-// const peruInd = (dataPeru) => {
-//   const newInd = [];
-//   for (let i = 0; i <dataPeru.length; i++){
-// console.log(dataPeru[i])
-//   newInd(Object.values(WORLDBANK.peru))
-// }
-// return newInd;
-// }
-
-
 const getDataPeru = (arrayDePaises) => {
   const dataDePeru = arrayDePaises[0]; // un objeto con 2 propiedades que tiene la data de peru
   return dataDePeru['1'].indicators;
@@ -37,9 +27,32 @@ const getDataPeru = (arrayDePaises) => {
 
 const arrayDeIndicadoresDePeru = getDataPeru(data);
 
-console.log(arrayDeIndicadoresDePeru)
-// Esto funciona solo con la funcion de arriba
-// dataClone(allTheData);
+//console.log(arrayDeIndicadoresDePeru);//
+
+const comparaIndicadores = (indicat1,indicat2) => {
+  //extraer los nombres
+  let nombreInd1=indicat1.indicatorName;
+  let nombreInd2=indicat2.indicatorName;
+
+  //condiciones para ordenar alfabeticamente
+  if (nombreInd1<nombreInd2)
+    return -1;
+  if (nombreInd1>nombreInd2)
+    return +1;
+  if (nombreInd1===nombreInd2)
+    return 0;
+};
+
+arrayDeIndicadoresDePeru.sort(comparaIndicadores);
+
+let listaHTML = document.getElementById("list");//esto iria en el main.js
+for(let a=0;a<arrayDeIndicadoresDePeru.length;a++){
+  let indicador=arrayDeIndicadoresDePeru[a].indicatorName;//obtengo el nombre del indicador
+  let nodoIndicador = document.createTextNode(indicador);//creo el texto HTML con el nombre del indicador
+  let nodoEntradaLista = document.createElement("li");//creo la entrada de lista sin texto
+  nodoEntradaLista.appendChild(nodoIndicador);//inserto el texto(nombre del indicador) en la entrada de la lista
+  listaHTML.appendChild(nodoEntradaLista);//inserto la entrada de la lista ahora con el texto en el documento HTML
+}
 
 // const example = () => {
 // return 'example';
